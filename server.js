b3c4b45609d63e3d63c0c7ddc755eebc340f1c46
ahtishamdilawar <126797321+ahtishamdilawar@users.noreply.github.com>
2024-06-12 18:04:11 +0500

    removed unused code

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const UserModel = require("./models/UserSchema.js");
-const RoleModel = require("./models/RoleSchema.js");
-const ExamModel = require("./models/Exam.js");
-const QuestionModel = require("./models/QuestionSchema.js");
-const SubmissionModel = require("./models/Submission.js");
-const ResultModel = require("./models/Result.js");
+
 const authRouter = require("./routes/auth.js");
 dotenv.config();
 console.log(process.env.MONGOSTR);
