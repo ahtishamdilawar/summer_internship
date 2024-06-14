3b1fc1254f3f285b54a95ed18bef3d0a4c196f8c
ahtishamdilawar <126797321+ahtishamdilawar@users.noreply.github.com>
2024-06-14 12:11:40 +0500

    get specific exam

--- controllers/students/getExams.js
+++ controllers/students/getExams.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 const ExamsModel = require("../../models/ExamsSchema.js");
+const QuestionModel = require("../../models/QuestionSchema.js");
 const authenticateUser = require("../../middleware/authenticateUser.js");
 const getRole = require("../../middleware/roles.js");
 
@@ -13,6 +14,30 @@ router.get("/", authenticateUser, getRole, async (req, res) => {
     res.status(200).json({ exams });
   } catch (error) {
     res.status(500).json({ message: "Error fetching exams", error });
+  }
+});
+
+router.get("/:examId", authenticateUser, getRole, async (req, res) => {
+  if (res.locals.role !== "Student") return res.status(403).send("Forbidden");
+
+  const { examId } = req.params;
+
+  try {
+    const exam = await ExamsModel.findOne({ _id: examId, status: "published" });
+
+    if (!exam) {
+      return res
+        .status(404)
+        .json({ message: "Exam not found or not accessible" });
+    }
+
+    const questions = await QuestionModel.find({ examID: examId });
+
+    const examWithQuestions = { ...exam.toObject(), questions };
+
+    res.status(200).json({ exam: examWithQuestions });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching exam details", error });
   }
 });
 
